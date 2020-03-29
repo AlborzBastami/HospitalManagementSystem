4a1df1b60a4506233a6f933172e29feadbb3c07c
Alborz Bastami <albor@DESKTOP-480NHN1.fritz.box>
2020-03-29 17:41:41 +0200

    Added Login and new MainWindow Constructor

--- src/hospitalmanagementsystem/Login.java
+++ src/hospitalmanagementsystem/Login.java
@@ -6,10 +6,31 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.JTabbedPane;
+import javax.swing.border.EtchedBorder;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
+import javax.swing.DefaultComboBoxModel;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textFieldUsername;
+	private JPasswordField textFieldPassword;
 
 	/**
 	 * Launch the application.
@@ -27,16 +48,148 @@ public class Login extends JFrame {
 		});
 	}
 
+	Connection con;
+	PreparedStatement pst;
+	ResultSet rs;
+
+	/**
+	 * MYSQL Connector
+	 */
+	public void Connect() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost/hospital", "root", "");
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * Create the frame.
 	 */
 	public Login() {
+		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1704, 1139);
+		setBounds(100, 100, 838, 521);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
 
+		JPanel panel = new JPanel();
+		Font titleFont = new Font("Arial", Font.TRUETYPE_FONT, 18);
+		panel.setBorder(new TitledBorder(null, "Login", TitledBorder.LEFT, TitledBorder.TOP, titleFont, null));
+		panel.setBounds(10, 11, 804, 462);
+		contentPane.add(panel);
+		panel.setLayout(null);
+
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				dispose();
+			}
+		});
+		btnCancel.setFont(new Font("Arial", Font.PLAIN, 18));
+		btnCancel.setBounds(208, 396, 188, 55);
+		panel.add(btnCancel);
+
+		JLabel lblNewLabel_1_1 = new JLabel("Username:");
+		lblNewLabel_1_1.setFont(new Font("Arial", Font.PLAIN, 15));
+		lblNewLabel_1_1.setBounds(10, 56, 81, 48);
+		panel.add(lblNewLabel_1_1);
+
+		textFieldUsername = new JTextField();
+		textFieldUsername.setFont(new Font("Arial", Font.PLAIN, 14));
+		textFieldUsername.setColumns(10);
+		textFieldUsername.setBounds(101, 66, 295, 30);
+		panel.add(textFieldUsername);
+
+		textFieldPassword = new JPasswordField();
+		textFieldPassword.setFont(new Font("Arial", Font.PLAIN, 14));
+		textFieldPassword.setColumns(10);
+		textFieldPassword.setBounds(101, 125, 295, 30);
+		panel.add(textFieldPassword);
+
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setFont(new Font("Arial", Font.PLAIN, 15));
+		lblPassword.setBounds(10, 115, 81, 48);
+		panel.add(lblPassword);
+
+		JLabel lblUsertype = new JLabel("Usertype:");
+		lblUsertype.setFont(new Font("Arial", Font.PLAIN, 15));
+		lblUsertype.setBounds(10, 174, 81, 48);
+		panel.add(lblUsertype);
+
+		JComboBox ComboBoxUserType = new JComboBox();
+		ComboBoxUserType.setModel(new DefaultComboBoxModel(new String[] { "Pharmcist", "Doctor", "Receptionist" }));
+		ComboBoxUserType.setFont(new Font("Arial", Font.PLAIN, 14));
+		ComboBoxUserType.setBounds(101, 184, 295, 30);
+		panel.add(ComboBoxUserType);
+
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String username;
+				String password;
+				String usertype;
+
+				// Building String for Login Check
+				username = textFieldUsername.getText();
+				password = textFieldPassword.getText();
+				usertype = ComboBoxUserType.getSelectedItem().toString();
+
+				Connect();
+
+				// Search the database for username and password matching
+				/*
+				 * SELECT * FROM USER WHERE username="The username entered" AND
+				 * password="The password entered" AND utype="The usertype selected"
+				 * 
+				 * The verification is done fully in mysql
+				 */
+
+				try {
+					// #1 Build the search Query
+					pst = con.prepareStatement("select * from user where username=? and password=? and utype=?");
+					pst.setString(1, username);
+					pst.setString(2, password);
+					pst.setString(3, usertype);
+
+					// #2 Run the Query
+					rs = pst.executeQuery();
+
+					// #3 if ResultSet = true
+					if (rs.next()) {
+						int userid = rs.getInt("id");
+
+						// close this window
+						setVisible(false);
+
+						// Construct a new Main Window and transmit login credentials
+						new Main(userid, username, usertype);
+					} else {
+
+						// If User Login was not successful
+						JOptionPane.showMessageDialog(null, "Username or Password wrong");
+						textFieldUsername.setText("");
+						textFieldPassword.setText("");
+						ComboBoxUserType.setSelectedIndex(-1);
+						textFieldUsername.requestFocus();
+					}
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+
+			}
+		});
+		btnLogin.setFont(new Font("Arial", Font.PLAIN, 18));
+		btnLogin.setBounds(10, 396, 188, 55);
+		panel.add(btnLogin);
+	}
 }
--- src/hospitalmanagementsystem/Main.java
+++ src/hospitalmanagementsystem/Main.java
@@ -7,6 +7,7 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -31,58 +32,71 @@ public class Main {
 		});
 	}
 
+	int u_id;
+	String u_name, u_type;
+
 	/**
 	 * Create the application.
 	 */
 	public Main() {
 		initialize();
 	}
+	
+	public Main(int userid, String username, String usertype) {
+
+		this.u_id = userid;
+		this.u_name = username;
+		this.u_type = usertype;
+		initialize(username, usertype);
+		
+	}
 
 	/**
 	 * Initialize the contents of the frame.
+	 * Default window, if NO USER is logged in
 	 */
-	private void initialize() {
+	public void initialize() {
 		frmHospitalManagementSystem = new JFrame();
 		frmHospitalManagementSystem.setTitle("Hospital Management System");
 		frmHospitalManagementSystem.setBounds(100, 100, 1722, 1079);
 		frmHospitalManagementSystem.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmHospitalManagementSystem.getContentPane().setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBounds(10, 11, 396, 1020);
 		frmHospitalManagementSystem.getContentPane().add(panel);
 		panel.setLayout(null);
-		
+
 		JButton btnPatient = new JButton("Patient");
 		btnPatient.setFont(new Font("Arial", Font.PLAIN, 18));
 		btnPatient.setBounds(10, 11, 376, 55);
 		panel.add(btnPatient);
-		
+
 		JButton btnDoctor = new JButton("Doctor");
 		btnDoctor.setFont(new Font("Arial", Font.PLAIN, 18));
 		btnDoctor.setBounds(10, 77, 376, 55);
 		panel.add(btnDoctor);
-		
+
 		JButton btnCreateChannel = new JButton("Create Channel");
 		btnCreateChannel.setFont(new Font("Arial", Font.PLAIN, 18));
 		btnCreateChannel.setBounds(10, 179, 376, 55);
 		panel.add(btnCreateChannel);
-		
+
 		JButton btnViewChannel = new JButton("View Channel");
 		btnViewChannel.setFont(new Font("Arial", Font.PLAIN, 18));
 		btnViewChannel.setBounds(10, 245, 376, 55);
 		panel.add(btnViewChannel);
-		
+
 		JButton btnViewPrescription = new JButton("View Prescription");
 		btnViewPrescription.setFont(new Font("Arial", Font.PLAIN, 18));
 		btnViewPrescription.setBounds(10, 347, 376, 55);
 		panel.add(btnViewPrescription);
-		
+
 		JButton btnCreateItem = new JButton("Create Item");
 		btnCreateItem.setFont(new Font("Arial", Font.PLAIN, 18));
 		btnCreateItem.setBounds(10, 449, 376, 55);
 		panel.add(btnCreateItem);
-		
+
 		JButton btnDoctor_4_1 = new JButton("Create User");
 		btnDoctor_4_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -93,51 +107,162 @@ public class Main {
 		btnDoctor_4_1.setFont(new Font("Arial", Font.PLAIN, 18));
 		btnDoctor_4_1.setBounds(10, 515, 376, 55);
 		panel.add(btnDoctor_4_1);
-		
+
 		JButton btnDoctor_4_2 = new JButton("View Doctor");
 		btnDoctor_4_2.setFont(new Font("Arial", Font.PLAIN, 18));
 		btnDoctor_4_2.setBounds(10, 617, 376, 55);
 		panel.add(btnDoctor_4_2);
-		
+
 		JButton btnDoctor_4_1_1 = new JButton("Logout");
 		btnDoctor_4_1_1.setFont(new Font("Arial", Font.PLAIN, 18));
 		btnDoctor_4_1_1.setBounds(10, 719, 376, 55);
 		panel.add(btnDoctor_4_1_1);
-		
+
 		JPanel panel_1 = new JPanel();
 		panel_1.setBounds(416, 11, 1282, 151);
 		frmHospitalManagementSystem.getContentPane().add(panel_1);
 		panel_1.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Hospital Management System");
 		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 25));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBounds(405, 49, 472, 53);
 		panel_1.add(lblNewLabel);
-		
+
 		JPanel panel_2 = new JPanel();
 		panel_2.setBounds(416, 173, 641, 248);
 		frmHospitalManagementSystem.getContentPane().add(panel_2);
 		panel_2.setLayout(null);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Username:");
 		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 15));
 		lblNewLabel_1.setBounds(10, 11, 100, 48);
 		panel_2.add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_1_1 = new JLabel("Usertype:");
 		lblNewLabel_1_1.setFont(new Font("Arial", Font.PLAIN, 15));
 		lblNewLabel_1_1.setBounds(10, 70, 100, 48);
 		panel_2.add(lblNewLabel_1_1);
-		
+
 		JLabel LabelUserName = new JLabel("<UserNameField>");
 		LabelUserName.setFont(new Font("Arial", Font.PLAIN, 15));
 		LabelUserName.setBounds(120, 11, 511, 48);
 		panel_2.add(LabelUserName);
-		
+
 		JLabel LabelUserType = new JLabel("<UserTypeField>");
 		LabelUserType.setFont(new Font("Arial", Font.PLAIN, 15));
 		LabelUserType.setBounds(120, 70, 511, 48);
 		panel_2.add(LabelUserType);
 	}
+
+	/**
+	 * @param username
+	 * @param usertype
+	 * Actual Window, WITH USER logged in
+	 */
+	public void initialize(String username, String usertype) {
+
+		frmHospitalManagementSystem = new JFrame();
+		frmHospitalManagementSystem.setTitle("Hospital Management System");
+		frmHospitalManagementSystem.setBounds(100, 100, 1722, 1079);
+		frmHospitalManagementSystem.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmHospitalManagementSystem.getContentPane().setLayout(null);
+		frmHospitalManagementSystem.setVisible(true);
+
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 11, 396, 1020);
+		frmHospitalManagementSystem.getContentPane().add(panel);
+		panel.setLayout(null);
+
+		JButton btnPatient = new JButton("Patient");
+		btnPatient.setFont(new Font("Arial", Font.PLAIN, 18));
+		btnPatient.setBounds(10, 11, 376, 55);
+		panel.add(btnPatient);
+
+		JButton btnDoctor = new JButton("Doctor");
+		btnDoctor.setFont(new Font("Arial", Font.PLAIN, 18));
+		btnDoctor.setBounds(10, 77, 376, 55);
+		panel.add(btnDoctor);
+
+		JButton btnCreateChannel = new JButton("Create Channel");
+		btnCreateChannel.setFont(new Font("Arial", Font.PLAIN, 18));
+		btnCreateChannel.setBounds(10, 179, 376, 55);
+		panel.add(btnCreateChannel);
+
+		JButton btnViewChannel = new JButton("View Channel");
+		btnViewChannel.setFont(new Font("Arial", Font.PLAIN, 18));
+		btnViewChannel.setBounds(10, 245, 376, 55);
+		panel.add(btnViewChannel);
+
+		JButton btnViewPrescription = new JButton("View Prescription");
+		btnViewPrescription.setFont(new Font("Arial", Font.PLAIN, 18));
+		btnViewPrescription.setBounds(10, 347, 376, 55);
+		panel.add(btnViewPrescription);
+
+		JButton btnCreateItem = new JButton("Create Item");
+		btnCreateItem.setFont(new Font("Arial", Font.PLAIN, 18));
+		btnCreateItem.setBounds(10, 449, 376, 55);
+		panel.add(btnCreateItem);
+
+		JButton btnDoctor_4_1 = new JButton("Create User");
+		btnDoctor_4_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				User u = new User();
+				u.setVisible(true);
+			}
+		});
+		btnDoctor_4_1.setFont(new Font("Arial", Font.PLAIN, 18));
+		btnDoctor_4_1.setBounds(10, 515, 376, 55);
+		panel.add(btnDoctor_4_1);
+
+		JButton btnDoctor_4_2 = new JButton("View Doctor");
+		btnDoctor_4_2.setFont(new Font("Arial", Font.PLAIN, 18));
+		btnDoctor_4_2.setBounds(10, 617, 376, 55);
+		panel.add(btnDoctor_4_2);
+
+		JButton btnDoctor_4_1_1 = new JButton("Logout");
+		btnDoctor_4_1_1.setFont(new Font("Arial", Font.PLAIN, 18));
+		btnDoctor_4_1_1.setBounds(10, 719, 376, 55);
+		panel.add(btnDoctor_4_1_1);
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(416, 11, 1282, 151);
+		frmHospitalManagementSystem.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+
+		JLabel lblNewLabel = new JLabel("Hospital Management System");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 25));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(405, 49, 472, 53);
+		panel_1.add(lblNewLabel);
+
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(416, 173, 641, 248);
+		frmHospitalManagementSystem.getContentPane().add(panel_2);
+		panel_2.setLayout(null);
+
+		JLabel lblNewLabel_1 = new JLabel("Username:");
+		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(10, 11, 100, 48);
+		panel_2.add(lblNewLabel_1);
+
+		JLabel lblNewLabel_1_1 = new JLabel("Usertype:");
+		lblNewLabel_1_1.setFont(new Font("Arial", Font.PLAIN, 15));
+		lblNewLabel_1_1.setBounds(10, 70, 100, 48);
+		panel_2.add(lblNewLabel_1_1);
+
+		JLabel LabelUserName = new JLabel("<UserNameField>");
+		LabelUserName.setFont(new Font("Arial", Font.PLAIN, 15));
+		LabelUserName.setBounds(120, 11, 511, 48);
+		LabelUserName.setText(username);
+		panel_2.add(LabelUserName);
+
+		JLabel LabelUserType = new JLabel("<UserTypeField>");
+		LabelUserType.setFont(new Font("Arial", Font.PLAIN, 15));
+		LabelUserType.setBounds(120, 70, 511, 48);
+		LabelUserType.setText(usertype);
+		panel_2.add(LabelUserType);
+	}
+
+	
 }
